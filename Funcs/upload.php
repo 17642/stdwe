a72487e9e667158ac42dcc4738df1e3c10992f22
17642 <67950060+17642@users.noreply.github.com>
2022-02-27 23:01:18 +0900

    Update upload.php

--- Funcs/upload.php
+++ Funcs/upload.php
@@ -9,6 +9,24 @@ $error=$_FILES['pfile']['error'];
 $name=$_FILES['pfile']['name'];
 $ext=array_pop(explode('.',$name));
 
+function getCurrentFilelist($dir){
+    $handle=opendir($dir);
+    $R=array();
+    while($filename=readdir($handle)){
+        if($filename=='.'||$filename=='..'||$filename=='download.php')continue;
+        $filepath=$dir.'/'.$filename;
+        if(is_file($filepath)){
+            $getExt=pathinfo($filename,PATHINFO_EXTENSION);
+            if(!in_array($getExt,array('.php','.html','.jsp','.net'))){
+                array_push($R,basename($filename));
+            }
+        }
+    }
+    closedir($handle);
+    sort($R);
+    return $R;
+}
+
 if($error!=UPLOAD_ERR_OK){
     switch($error){
         case UPLOAD_ERR_INI_SIZE:
@@ -31,6 +49,12 @@ if(in_array($ext,$extnacpted)){
     header('location:',$_SERVER['HTTP_REFFER']);
     exit;
 }
+if (in_array($name,getCurrentFilelist($DIR_PATH))){
+    echo "<script>alert('This file may already exist.')</script>";
+    header('location:',$_SERVER['HTTP_REFFER']);
+    exit;
+}
+
 
 move_uploaded_file($_FILES['pfile']['tmp_name'],"$DIR_PATH/$name");
 ?>
