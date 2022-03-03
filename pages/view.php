c6bdb672662e320d3c1655d968832dd8fa2206ee
17642 <67950060+17642@users.noreply.github.com>
2022-03-03 22:34:29 +0900

    Update view.php

--- pages/view.php
+++ pages/view.php
@@ -1,4 +1,16 @@
 <?php
 require("../head.php");
-$mplink="./";
+$ddbb=new mysqli("localhost","root","1029a382","./pages.db");
+if($ddbb->connect_error){
+  die("connect failed".$ddbb->connect_error);
+  exit;
+}
 ?>
+<html>
+  <head>
+    <title><?php
+      echo "post $_GET["num"]";
+      ?>
+    </title>
+  </head>
+</html>
