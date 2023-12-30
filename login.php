d85be9031a8e28a46df7d328b0170cd98fedb94a
상빈 <67950060+17642@users.noreply.github.com>
2023-12-30 10:53:30 +0900

    Update login.php

--- login.php
+++ login.php
@@ -1,11 +1,9 @@
 <?php
 if(!isset($_POST['pwd'])) exit;
 $pwd=$_POST['pwd'];
-$spwd=fopen("./PWD.DB","r") or die("ERROR");
-$pwds=fgets($spwd);
-fclose($spwd);
-if($pwds!=$pwd){
-    echo "<script>alert('I don't think this password is correct')</script>";
+$spwd=getenv('SSERV_KEY') or die("ERROR");
+if($spwd!=$pwd){
+    echo "<script>alert('I don\'t think this password is correct')</script>";
     header('location:',$_SERVER['HTTP_REFFER']);
     exit;
 }
